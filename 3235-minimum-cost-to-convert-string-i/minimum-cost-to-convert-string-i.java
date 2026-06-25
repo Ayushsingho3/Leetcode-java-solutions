6658a0980b7cf93b3b39d0ecceee61b4e02533fe
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-06-25 21:40:31 +0530

    Time: 19 ms (79.26%) | Memory: 48 MB (45.01%) - LeetSync

---
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+class Solution {
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        long[][] dist = new long[26][26];
+        long INF = Long.MAX_VALUE / 2; 
+        
+        for (int i = 0; i < 26; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+        
+        for (int i = 0; i < original.length; i++) {
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
+            dist[u][v] = Math.min(dist[u][v], (long) cost[i]);
+        }
+        
+        for (int k = 0; k < 26; k++) {
+            for (int i = 0; i < 26; i++) {
+                if (dist[i][k] == INF) continue;
+                for (int j = 0; j < 26; j++) {
+                    if (dist[k][j] == INF) continue;
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        
+        long totalCost = 0;
+        int n = source.length();
+        
+        for (int i = 0; i < n; i++) {
+            if (source.charAt(i) == target.charAt(i)) {
+                continue;
+            }
+            
+            int u = source.charAt(i) - 'a';
+            int v = target.charAt(i) - 'a';
+            
+            if (dist[u][v] == INF) {
+                return -1;
+            }
+            
+            totalCost += dist[u][v];
+        }
+        
+        return totalCost;
+    }
+}
