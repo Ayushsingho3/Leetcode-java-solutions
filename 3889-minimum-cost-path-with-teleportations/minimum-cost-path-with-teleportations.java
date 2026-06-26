66921fc66adf2dd6e1c627c701905ddd3678ad85
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-06-26 23:03:13 +0530

    Time: 1306 ms (7.95%) | Memory: 99.6 MB (5.11%) - LeetSync

--- 3889-minimum-cost-path-with-teleportations/minimum-cost-path-with-teleportations.java
+++ 3889-minimum-cost-path-with-teleportations/minimum-cost-path-with-teleportations.java
@@ -1,105 +1,62 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    class Cell implements Comparable<Cell> {
-        int r, c, val;
-        
-        public Cell(int r, int c, int val) {
-            this.r = r;
-            this.c = c;
-            this.val = val;
-        }
-        
-        @Override
-        public int compareTo(Cell other) {
-            return Integer.compare(other.val, this.val);
-        }
-    }
-
     public int minCost(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
+        long INF = Long.MAX_VALUE / 2; 
         
-        long[][] dp = new long[m][n];
+        long[][][] dp = new long[k + 1][m][n];
+        for (int t = 0; t <= k; t++) {
+            for (int i = 0; i < m; i++) {
+                Arrays.fill(dp[t][i], INF);
+            }
+        }
+        
+        dp[0][0][0] = 0;
+        
         for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], Long.MAX_VALUE);
+            for (int j = 0; j < n; j++) {
+                if (i > 0) dp[0][i][j] = Math.min(dp[0][i][j], dp[0][i - 1][j] + grid[i][j]);
+                if (j > 0) dp[0][i][j] = Math.min(dp[0][i][j], dp[0][i][j - 1] + grid[i][j]);
+            }
         }
         
-        dp[0][0] = 0;
+        TreeMap<Integer, List<int[]>> valueGroups = new TreeMap<>(Collections.reverseOrder());
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                valueGroups.computeIfAbsent(grid[i][j], x -> new ArrayList<>()).add(new int[]{i, j});
+            }
+        }
         
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (r == 0 && c == 0) continue;
+        for (int t = 1; t <= k; t++) {
+            long minCostFromLargerCells = INF;
+            
+            for (int val : valueGroups.keySet()) {
+                List<int[]> cells = valueGroups.get(val);
                 
-                long minPrev = Long.MAX_VALUE;
-                if (r > 0) minPrev = Math.min(minPrev, dp[r-1][c]);
-                if (c > 0) minPrev = Math.min(minPrev, dp[r][c-1]);
+                for (int[] cell : cells) {
+                    minCostFromLargerCells = Math.min(minCostFromLargerCells, dp[t - 1][cell[0]][cell[1]]);
+                }
                 
-                if (minPrev != Long.MAX_VALUE) {
-                    dp[r][c] = minPrev + grid[r][c];
+                for (int[] cell : cells) {
+                    dp[t][cell[0]][cell[1]] = minCostFromLargerCells;
+                }
+            }
+            
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i > 0) dp[t][i][j] = Math.min(dp[t][i][j], dp[t][i - 1][j] + grid[i][j]);
+                    if (j > 0) dp[t][i][j] = Math.min(dp[t][i][j], dp[t][i][j - 1] + grid[i][j]);
                 }
             }
         }
         
-        Cell[] cells = new Cell[m * n];
-        int idx = 0;
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                cells[idx++] = new Cell(r, c, grid[r][c]);
-            }
-        }
-        Arrays.sort(cells);
-        
-        int maxTeleports = Math.min(k, m * n);
-        
-        for (int t = 1; t <= maxTeleports; t++) {
-            long[][] next_dp = new long[m][n];
-            long min_dp = Long.MAX_VALUE;
-            
-            int i = 0;
-            while (i < cells.length) {
-                int j = i;
-                long min_in_group = Long.MAX_VALUE;
-                
-                while (j < cells.length && cells[j].val == cells[i].val) {
-                    min_in_group = Math.min(min_in_group, dp[cells[j].r][cells[j].c]);
-                    j++;
-                }
-                
-                min_dp = Math.min(min_dp, min_in_group);
-                
-                for (int p = i; p < j; p++) {
-                    next_dp[cells[p].r][cells[p].c] = min_dp;
-                }
-                
-                i = j;
-            }
-            
-            boolean changed = false;
-            
-            for (int r = 0; r < m; r++) {
-                for (int c = 0; c < n; c++) {
-                    long minPrev = Long.MAX_VALUE;
-                    if (r > 0) minPrev = Math.min(minPrev, next_dp[r-1][c]);
-                    if (c > 0) minPrev = Math.min(minPrev, next_dp[r][c-1]);
-                    
-                    if (minPrev != Long.MAX_VALUE) {
-                        next_dp[r][c] = Math.min(next_dp[r][c], minPrev + grid[r][c]);
-                    }
-                    
-                    if (next_dp[r][c] != dp[r][c]) {
-                        changed = true;
-                    }
-                }
-            }
-            
-            if (!changed) {
-                break;
-            }
-            
-            dp = next_dp;
+        long minTotalCost = INF;
+        for (int t = 0; t <= k; t++) {
+            minTotalCost = Math.min(minTotalCost, dp[t][m - 1][n - 1]);
         }
         
-        return dp[m-1][n-1] == Long.MAX_VALUE ? -1 : (int) dp[m-1][n-1];
+        return (int) minTotalCost;
     }
 }
