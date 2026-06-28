cf1b02c6e040db329048e951646696c44dae9510
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-06-28 22:25:58 +0530

    Time: 465 ms (9.33%) | Memory: 96 MB (59.33%) - LeetSync

--- 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.java
+++ 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.java
@@ -1,54 +1,81 @@
-import java.util.TreeSet;
+import java.util.TreeMap;
 
 class Solution {
     public long minimumCost(int[] nums, int k, int dist) {
+        Window win = new Window(k - 1);
         int n = nums.length;
-        int k_needed = k - 1;
         
-        TreeSet<Integer> left = new TreeSet<>((a, b) -> nums[a] == nums[b] ? a - b : Integer.compare(nums[a], nums[b]));
-        TreeSet<Integer> right = new TreeSet<>((a, b) -> nums[a] == nums[b] ? a - b : Integer.compare(nums[a], nums[b]));
-        
-        long currentSum = 0;
-        
-        int windowEnd = Math.min(n - 1, 1 + dist);
-        for (int i = 1; i <= windowEnd; i++) {
-            left.add(i);
-            currentSum += nums[i];
-            if (left.size() > k_needed) {
-                int max_left = left.pollLast();
-                currentSum -= nums[max_left];
-                right.add(max_left);
-            }
+        for (int i = 1; i <= Math.min(dist + 1, n - 1); i++) {
+            win.add(nums[i]);
         }
         
-        long minSum = currentSum;
+        long minCost = win.leftSum;
         
-        for (int i = windowEnd + 1; i < n; i++) {
-            int outIndex = i - dist - 1;
-            
-            if (right.contains(outIndex)) {
-                right.remove(outIndex);
+        for (int i = dist + 2; i < n; i++) {
+            win.add(nums[i]);
+            win.remove(nums[i - (dist + 1)]);
+            minCost = Math.min(minCost, win.leftSum);
+        }
+        
+        return nums[0] + minCost;
+    }
+    
+    class Window {
+        TreeMap<Integer, Integer> left = new TreeMap<>();
+        TreeMap<Integer, Integer> right = new TreeMap<>();
+        int leftCount = 0;
+        int rightCount = 0;
+        long leftSum = 0;
+        int k;
+
+        public Window(int k) {
+            this.k = k;
+        }
+
+        public void add(int val) {
+            left.put(val, left.getOrDefault(val, 0) + 1);
+            leftCount++;
+            leftSum += val;
+
+            if (leftCount > k) {
+                int maxLeft = left.lastKey();
+                removeFromMap(left, maxLeft);
+                leftCount--;
+                leftSum -= maxLeft;
+                
+                right.put(maxLeft, right.getOrDefault(maxLeft, 0) + 1);
+                rightCount++;
+            }
+        }
+
+        public void remove(int val) {
+            if (right.containsKey(val)) {
+                removeFromMap(right, val);
+                rightCount--;
             } else {
-                left.remove(outIndex);
-                currentSum -= nums[outIndex];
-                if (!right.isEmpty()) {
-                    int min_right = right.pollFirst();
-                    left.add(min_right);
-                    currentSum += nums[min_right];
-                }
+                removeFromMap(left, val);
+                leftCount--;
+                leftSum -= val;
             }
-            
-            left.add(i);
-            currentSum += nums[i];
-            if (left.size() > k_needed) {
-                int max_left = left.pollLast();
-                currentSum -= nums[max_left];
-                right.add(max_left);
+
+            if (leftCount < k && rightCount > 0) {
+                int minRight = right.firstKey();
+                removeFromMap(right, minRight);
+                rightCount--;
+                
+                left.put(minRight, left.getOrDefault(minRight, 0) + 1);
+                leftCount++;
+                leftSum += minRight;
             }
-            
-            minSum = Math.min(minSum, currentSum);
         }
-        
-        return nums[0] + minSum;
+
+        private void removeFromMap(TreeMap<Integer, Integer> map, int val) {
+            int count = map.get(val);
+            if (count == 1) {
+                map.remove(val);
+            } else {
+                map.put(val, count - 1);
+            }
+        }
     }
 }
