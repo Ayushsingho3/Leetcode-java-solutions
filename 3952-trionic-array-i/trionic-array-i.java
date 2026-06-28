48708d64cddecb87e884becab2b8b7d060d15578
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-06-28 22:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (16.60%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -1,28 +1,40 @@
 class Solution {
     public boolean isTrionic(int[] nums) {
-        int n = nums.length;
-        if (n < 4) return false;
-        
-        int i = 0;
-        
-        while (i < n - 1 && nums[i + 1] > nums[i]) {
-            i++;
+        if (nums.length < 4) {
+            return false;
         }
         
-        int p = i;
-        if (p == 0 || p == n - 1) return false;
+        int phase = 0;
         
-        while (i < n - 1 && nums[i + 1] < nums[i]) {
-            i++;
+        for (int i = 1; i < nums.length; i++) {
+            if (phase == 0) {
+                if (nums[i] > nums[i - 1]) {
+                    continue;
+                } else if (nums[i] < nums[i - 1]) {
+                    if (i == 1) {
+                        return false;
+                    }
+                    phase = 1;
+                } else {
+                    return false;
+                }
+            } else if (phase == 1) {
+                if (nums[i] < nums[i - 1]) {
+                    continue;
+                } else if (nums[i] > nums[i - 1]) {
+                    phase = 2;
+                } else {
+                    return false;
+                }
+            } else if (phase == 2) {
+                if (nums[i] > nums[i - 1]) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
         }
         
-        int q = i;
-        if (q == p || q == n - 1) return false;
-        
-        while (i < n - 1 && nums[i + 1] > nums[i]) {
-            i++;
-        }
-        
-        return i == n - 1;
+        return phase == 2;
     }
 }
