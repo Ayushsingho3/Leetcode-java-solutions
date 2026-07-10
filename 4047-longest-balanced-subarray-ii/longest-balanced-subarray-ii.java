789d8ea54971959c23827a4af3f4d8be7154c242
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-07-10 11:22:17 +0530

    Time: 169 ms (100.00%) | Memory: 69.9 MB (99.40%) - LeetSync

--- 4047-longest-balanced-subarray-ii/longest-balanced-subarray-ii.java
+++ 4047-longest-balanced-subarray-ii/longest-balanced-subarray-ii.java
@@ -1,53 +1,87 @@
 import java.util.Arrays;
 
 class Solution {
-    int[] minVal;
-    int[] maxVal;
-    int[] lazy;
+    private int[] minVal;
+    private int[] maxVal;
+    private int[] lazy;
 
     public int longestBalanced(int[] nums) {
         int n = nums.length;
         if (n == 0) return 0;
 
-        int treeSize = 4 * n + 1;
+        int treeSize = (n << 2) + 1;
         minVal = new int[treeSize];
         maxVal = new int[treeSize];
         lazy = new int[treeSize];
 
-        int[] temp = nums.clone();
-        Arrays.sort(temp);
-        int m = 0;
-        for (int i = 0; i < n; i++) {
-            if (i == 0 || temp[i] != temp[i - 1]) {
-                temp[m++] = temp[i];
-            }
+        int min = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < min) min = nums[i];
+            else if (nums[i] > max) max = nums[i];
         }
-        
-        int[] lastPos = new int[m];
-        Arrays.fill(lastPos, -1);
+
+        long range = (long) max - (long) min;
+        boolean direct = range <= 500000;
+
+        int[] lastPos;
+        int[] temp = null;
+        int m = 0;
+
+        if (direct) {
+            lastPos = new int[(int) range + 1];
+            Arrays.fill(lastPos, -1);
+        } else {
+            temp = nums.clone();
+            Arrays.sort(temp);
+            for (int i = 0; i < n; i++) {
+                if (i == 0 || temp[i] != temp[i - 1]) {
+                    temp[m++] = temp[i];
+                }
+            }
+            lastPos = new int[m];
+            Arrays.fill(lastPos, -1);
+        }
 
         int maxLen = 0;
 
         for (int i = 0; i < n; i++) {
             int num = nums[i];
-            int id = Arrays.binarySearch(temp, 0, m, num);
-            
+            int id;
+
+            if (direct) {
+                id = num - min;
+            } else {
+                int low = 0;
+                int high = m - 1;
+                id = 0;
+                while (low <= high) {
+                    int mid = (low + high) >>> 1;
+                    if (temp[mid] < num) {
+                        low = mid + 1;
+                    } else if (temp[mid] > num) {
+                        high = mid - 1;
+                    } else {
+                        id = mid;
+                        break;
+                    }
+                }
+            }
+
             int prev = lastPos[id];
             lastPos[id] = i;
-            
+
             int val = (num % 2 == 0) ? 1 : -1;
 
             if (prev + 1 <= i) {
                 update(1, 0, n - 1, prev + 1, i, val);
             }
 
-            if (minVal[1] <= 0 && maxVal[1] >= 0) {
-                int firstZero = queryFirstZero(1, 0, n - 1);
-                if (firstZero <= i) {
-                    int len = i - firstZero + 1;
-                    if (len > maxLen) {
-                        maxLen = len;
-                    }
+            int firstZero = queryFirstZero(1, 0, n - 1, i);
+            if (firstZero != -1) {
+                int len = i - firstZero + 1;
+                if (len > maxLen) {
+                    maxLen = len;
                 }
             }
         }
@@ -62,24 +96,24 @@ class Solution {
             lazy[node] += val;
             return;
         }
-        
-        int lz = lazy[node];
+
         int left = node << 1;
         int right = left | 1;
-        
+        int lz = lazy[node];
+
         if (lz != 0) {
             lazy[left] += lz;
             minVal[left] += lz;
             maxVal[left] += lz;
-            
+
             lazy[right] += lz;
             minVal[right] += lz;
             maxVal[right] += lz;
-            
+
             lazy[node] = 0;
         }
-        
-        int mid = (l + r) >> 1;
+
+        int mid = (l + r) >>> 1;
 
         if (ql <= mid) {
             update(left, l, mid, ql, qr, val);
@@ -88,37 +122,46 @@ class Solution {
             update(right, mid + 1, r, ql, qr, val);
         }
 
-        minVal[node] = minVal[left] < minVal[right] ? minVal[left] : minVal[right];
-        maxVal[node] = maxVal[left] > maxVal[right] ? maxVal[left] : maxVal[right];
+        int lMin = minVal[left];
+        int rMin = minVal[right];
+        minVal[node] = lMin < rMin ? lMin : rMin;
+
+        int lMax = maxVal[left];
+        int rMax = maxVal[right];
+        maxVal[node] = lMax > rMax ? lMax : rMax;
     }
 
-    private int queryFirstZero(int node, int l, int r) {
+    private int queryFirstZero(int node, int l, int r, int qr) {
+        if (l > qr || minVal[node] > 0 || maxVal[node] < 0) {
+            return -1;
+        }
         if (l == r) {
             return l;
         }
-        
-        int lz = lazy[node];
+
         int left = node << 1;
         int right = left | 1;
-        
+        int lz = lazy[node];
+
         if (lz != 0) {
             lazy[left] += lz;
             minVal[left] += lz;
             maxVal[left] += lz;
-            
+
             lazy[right] += lz;
             minVal[right] += lz;
             maxVal[right] += lz;
-            
+
             lazy[node] = 0;
         }
 
-        int mid = (l + r) >> 1;
+        int mid = (l + r) >>> 1;
 
-        if (minVal[left] <= 0 && maxVal[left] >= 0) {
-            return queryFirstZero(left, l, mid);
-        } else {
-            return queryFirstZero(right, mid + 1, r);
+        int res = queryFirstZero(left, l, mid, qr);
+        if (res != -1) {
+            return res;
         }
+
+        return queryFirstZero(right, mid + 1, r, qr);
     }
 }
