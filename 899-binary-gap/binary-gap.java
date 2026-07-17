cc374cef598456f50ad570032297d501de9ef599
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-07-17 21:29:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (70.07%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -1,18 +1,18 @@
 class Solution {
     public int binaryGap(int n) {
         int maxDistance = 0;
-        int prev = -1;
-        int i = 0;
+        int lastOnePos = -1;
+        int currentPos = 0;
         
         while (n > 0) {
             if ((n & 1) == 1) {
-                if (prev != -1) {
-                    maxDistance = Math.max(maxDistance, i - prev);
+                if (lastOnePos != -1) {
+                    maxDistance = Math.max(maxDistance, currentPos - lastOnePos);
                 }
-                prev = i;
+                lastOnePos = currentPos;
             }
+            currentPos++;
             n >>= 1;
-            i++;
         }
         
         return maxDistance;
