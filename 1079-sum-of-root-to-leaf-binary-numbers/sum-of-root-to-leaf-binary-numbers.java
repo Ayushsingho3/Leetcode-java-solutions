ffa4de8053c1ad4f90006d3f35f6c68f0b3550fd
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-07-19 09:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (86.13%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
@@ -17,18 +17,15 @@ class Solution {
     public int sumRootToLeaf(TreeNode root) {
         return dfs(root, 0);
     }
-    
-    private int dfs(TreeNode node, int currentSum) {
+
+    private int dfs(TreeNode node, int val) {
         if (node == null) {
             return 0;
         }
-        
-        currentSum = (currentSum << 1) | node.val;
-        
+        val = (val << 1) | node.val;
         if (node.left == null && node.right == null) {
-            return currentSum;
+            return val;
         }
-        
-        return dfs(node.left, currentSum) + dfs(node.right, currentSum);
+        return dfs(node.left, val) + dfs(node.right, val);
     }
 }
