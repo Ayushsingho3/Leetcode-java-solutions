ba6569bda94ffa90ca9b92c10e6a292932cf04ae
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-08-07 23:04:40 +0530

    Time: 11 ms (63.64%) | Memory: 47.4 MB (92.84%) - LeetSync

---
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -0,0 +1,48 @@
+class Solution {
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+        long low = 0;
+        long minW = workerTimes[0];
+        for (int w : workerTimes) {
+            minW = Math.min(minW, w);
+        }
+        
+        long high = minW * (long) mountainHeight * (mountainHeight + 1L) / 2L;
+        
+        long ans = high;
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            if (check(mid, mountainHeight, workerTimes)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+    
+    private boolean check(long T, int mountainHeight, int[] workerTimes) {
+        long reduced = 0;
+        for (int w : workerTimes) {
+            long maxVal = (2L * T) / w;
+            
+            long x = (long) Math.sqrt(maxVal);
+            
+            while (x * (x + 1) > maxVal) {
+                x--;
+            }
+            
+            while ((x + 1) * (x + 2) <= maxVal) {
+                x++;
+            }
+            
+            reduced += x;
+            
+            if (reduced >= mountainHeight) {
+                return true;
+            }
+        }
+        return reduced >= mountainHeight;
+    }
+}
