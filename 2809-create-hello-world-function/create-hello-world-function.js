f7fa60696cb35e738b96638db484fb460aba6a7f
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-08-09 21:47:38 +0530

    Time: 45 ms (39.92%) | Memory: 52.7 MB (91.13%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        
+    }
+};
+
+
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    };
+};
