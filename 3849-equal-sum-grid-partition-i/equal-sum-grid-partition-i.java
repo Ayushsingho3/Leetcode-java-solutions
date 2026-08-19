45257dedf83032ab0b76ddb7dff2f7c9f431deb9
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-08-19 23:11:57 +0530

    Time: 3 ms (100.00%) | Memory: 163.8 MB (14.92%) - LeetSync

---
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean canPartitionGrid(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        long[] rowSum = new long[m];
+        long[] colSum = new long[n];
+        long total = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                long val = grid[i][j];
+                rowSum[i] += val;
+                colSum[j] += val;
+                total += val;
+            }
+        }
+        
+        if (total % 2 != 0) {
+            return false;
+        }
+        
+        long target = total / 2;
+        
+        long current = 0;
+        for (int i = 0; i < m - 1; i++) {
+            current += rowSum[i];
+            if (current == target) {
+                return true;
+            }
+        }
+        
+        current = 0;
+        for (int j = 0; j < n - 1; j++) {
+            current += colSum[j];
+            if (current == target) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
