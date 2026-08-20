58f052f2c71c1a99ed3df55bca94e5f0faec0387
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-08-20 23:53:47 +0530

    Time: 476 ms (38.53%) | Memory: 279.4 MB (33.95%) - LeetSync

--- 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.java
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.java
@@ -1,116 +1,116 @@
 class Solution {
+    public boolean canPartition(int[][] grid) {
+        return canPartitionGrid(grid);
+    }
+
     public boolean canPartitionGrid(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        long totalSum = 0;
         
-        long[] rowSum = new long[m];
-        long[] colSum = new long[n];
-        long total = 0;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                long val = grid[i][j];
-                rowSum[i] += val;
-                colSum[j] += val;
-                total += val;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                totalSum += grid[r][c];
             }
         }
         
         if (m > 1) {
-            java.util.Set<Long> topSet = new java.util.HashSet<>();
             long topSum = 0;
-            for (int i = 0; i < m - 1; i++) {
-                for (int j = 0; j < n; j++) {
-                    topSet.add((long) grid[i][j]);
-                }
-                topSum += rowSum[i];
-                long bottomSum = total - topSum;
-                long d = topSum - bottomSum;
-                
-                if (d == 0) return true;
-                
-                if (d > 0) {
-                    if (n == 1) {
-                        if (d == grid[0][0] || d == grid[i][0]) return true;
-                    } else {
-                        if (i == 0) {
-                            if (d == grid[0][0] || d == grid[0][n - 1]) return true;
-                        } else {
-                            if (topSet.contains(d)) return true;
-                        }
-                    }
+            java.util.Map<Long, Integer> topMap = new java.util.HashMap<>();
+            java.util.Map<Long, Integer> bottomMap = new java.util.HashMap<>();
+            
+            for (int r = 0; r < m; r++) {
+                for (int c = 0; c < n; c++) {
+                    long val = grid[r][c];
+                    bottomMap.put(val, bottomMap.getOrDefault(val, 0) + 1);
                 }
             }
             
-            java.util.Set<Long> bottomSet = new java.util.HashSet<>();
-            long bottomSumRev = 0;
-            for (int i = m - 1; i >= 1; i--) {
-                for (int j = 0; j < n; j++) {
-                    bottomSet.add((long) grid[i][j]);
-                }
-                bottomSumRev += rowSum[i];
-                long topSumRev = total - bottomSumRev;
-                long d = bottomSumRev - topSumRev;
-                
-                if (d > 0) {
-                    if (n == 1) {
-                        if (d == grid[i][0] || d == grid[m - 1][0]) return true;
+            for (int i = 0; i < m - 1; i++) {
+                for (int c = 0; c < n; c++) {
+                    long val = grid[i][c];
+                    topSum += val;
+                    topMap.put(val, topMap.getOrDefault(val, 0) + 1);
+                    
+                    int count = bottomMap.get(val);
+                    if (count == 1) {
+                        bottomMap.remove(val);
                     } else {
-                        if (i == m - 1) {
-                            if (d == grid[m - 1][0] || d == grid[m - 1][n - 1]) return true;
-                        } else {
-                            if (bottomSet.contains(d)) return true;
-                        }
+                        bottomMap.put(val, count - 1);
+                    }
+                }
+                
+                long bottomSum = totalSum - topSum;
+                long diff = Math.abs(topSum - bottomSum);
+                
+                if (diff == 0) return true;
+                
+                if (topSum > bottomSum) {
+                    if (i == 0) {
+                        if (diff == grid[0][0] || diff == grid[0][n - 1]) return true;
+                    } else if (n == 1) {
+                        if (diff == grid[0][0] || diff == grid[i][0]) return true;
+                    } else {
+                        if (topMap.containsKey(diff)) return true;
+                    }
+                } else {
+                    if (i == m - 2) {
+                        if (diff == grid[m - 1][0] || diff == grid[m - 1][n - 1]) return true;
+                    } else if (n == 1) {
+                        if (diff == grid[i + 1][0] || diff == grid[m - 1][0]) return true;
+                    } else {
+                        if (bottomMap.containsKey(diff)) return true;
                     }
                 }
             }
         }
         
         if (n > 1) {
-            java.util.Set<Long> leftSet = new java.util.HashSet<>();
             long leftSum = 0;
-            for (int j = 0; j < n - 1; j++) {
-                for (int i = 0; i < m; i++) {
-                    leftSet.add((long) grid[i][j]);
-                }
-                leftSum += colSum[j];
-                long rightSum = total - leftSum;
-                long d = leftSum - rightSum;
-                
-                if (d == 0) return true;
-                
-                if (d > 0) {
-                    if (m == 1) {
-                        if (d == grid[0][0] || d == grid[0][j]) return true;
-                    } else {
-                        if (j == 0) {
-                            if (d == grid[0][0] || d == grid[m - 1][0]) return true;
-                        } else {
-                            if (leftSet.contains(d)) return true;
-                        }
-                    }
+            java.util.Map<Long, Integer> leftMap = new java.util.HashMap<>();
+            java.util.Map<Long, Integer> rightMap = new java.util.HashMap<>();
+            
+            for (int r = 0; r < m; r++) {
+                for (int c = 0; c < n; c++) {
+                    long val = grid[r][c];
+                    rightMap.put(val, rightMap.getOrDefault(val, 0) + 1);
                 }
             }
             
-            java.util.Set<Long> rightSet = new java.util.HashSet<>();
-            long rightSumRev = 0;
-            for (int j = n - 1; j >= 1; j--) {
-                for (int i = 0; i < m; i++) {
-                    rightSet.add((long) grid[i][j]);
-                }
-                rightSumRev += colSum[j];
-                long leftSumRev = total - rightSumRev;
-                long d = rightSumRev - leftSumRev;
-                
-                if (d > 0) {
-                    if (m == 1) {
-                        if (d == grid[0][j] || d == grid[0][n - 1]) return true;
+            for (int j = 0; j < n - 1; j++) {
+                for (int r = 0; r < m; r++) {
+                    long val = grid[r][j];
+                    leftSum += val;
+                    leftMap.put(val, leftMap.getOrDefault(val, 0) + 1);
+                    
+                    int count = rightMap.get(val);
+                    if (count == 1) {
+                        rightMap.remove(val);
                     } else {
-                        if (j == n - 1) {
-                            if (d == grid[0][n - 1] || d == grid[m - 1][n - 1]) return true;
-                        } else {
-                            if (rightSet.contains(d)) return true;
-                        }
+                        rightMap.put(val, count - 1);
+                    }
+                }
+                
+                long rightSum = totalSum - leftSum;
+                long diff = Math.abs(leftSum - rightSum);
+                
+                if (diff == 0) return true;
+                
+                if (leftSum > rightSum) {
+                    if (j == 0) {
+                        if (diff == grid[0][0] || diff == grid[m - 1][0]) return true;
+                    } else if (m == 1) {
+                        if (diff == grid[0][0] || diff == grid[0][j]) return true;
+                    } else {
+                        if (leftMap.containsKey(diff)) return true;
+                    }
+                } else {
+                    if (j == n - 2) {
+                        if (diff == grid[0][n - 1] || diff == grid[m - 1][n - 1]) return true;
+                    } else if (m == 1) {
+                        if (diff == grid[0][j + 1] || diff == grid[0][n - 1]) return true;
+                    } else {
+                        if (rightMap.containsKey(diff)) return true;
                     }
                 }
             }
