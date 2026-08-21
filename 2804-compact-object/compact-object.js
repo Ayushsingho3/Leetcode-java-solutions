306805d807a6cf3275eddb5491ba9d10ab3ad5f7
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-08-21 23:13:13 +0530

    Time: 70 ms (56.34%) | Memory: 65.5 MB (58.90%) - LeetSync

---
+++ 2804-compact-object/compact-object.js
@@ -0,0 +1,24 @@
+var compactObject = function(obj) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    
+    if (Array.isArray(obj)) {
+        const compactedArr = [];
+        for (let i = 0; i < obj.length; i++) {
+            if (Boolean(obj[i])) {
+                compactedArr.push(compactObject(obj[i]));
+            }
+        }
+        return compactedArr;
+    }
+    
+    const compactedObj = {};
+    for (const key in obj) {
+        if (Boolean(obj[key])) {
+            compactedObj[key] = compactObject(obj[key]);
+        }
+    }
+    
+    return compactedObj;
+};
