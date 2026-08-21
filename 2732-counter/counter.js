c8a548794e04c547c36ed117f9089e6260d8bd73
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-08-21 23:20:47 +0530

    Time: 37 ms (84.89%) | Memory: 53.4 MB (67.91%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -1,14 +1,3 @@
-/**
- * @param {number} n
- * @return {Function} counter
- */
-var createCounter = function(n) {
-    
-    return function() {
-        
-    };
-};
-
 var createCounter = function(n) {
     return function() {
         return n++;
