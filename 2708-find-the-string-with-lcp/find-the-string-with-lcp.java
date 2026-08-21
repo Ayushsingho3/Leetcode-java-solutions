57c6efb6f54b95db60fd25a72e392a02a0866c2b
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-08-21 23:10:00 +0530

    Time: 6 ms (50.00%) | Memory: 172.2 MB (75.00%) - LeetSync

---
+++ 2708-find-the-string-with-lcp/find-the-string-with-lcp.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String findTheString(int[][] lcp) {
+        int n = lcp.length;
+        char[] word = new char[n];
+        char currentChar = 'a';
+        
+        for (int i = 0; i < n; i++) {
+            if (word[i] == 0) {
+                if (currentChar > 'z') {
+                    return "";
+                }
+                for (int j = i; j < n; j++) {
+                    if (lcp[i][j] > 0) {
+                        if (word[j] == 0) {
+                            word[j] = currentChar;
+                        }
+                    }
+                }
+                currentChar++;
+            }
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int expected = 0;
+                
+                if (word[i] == word[j]) {
+                    expected = 1 + ((i + 1 < n && j + 1 < n) ? lcp[i + 1][j + 1] : 0);
+                }
+                
+                if (lcp[i][j] != expected) {
+                    return "";
+                }
+            }
+        }
+        
+        return new String(word);
+    }
+}
