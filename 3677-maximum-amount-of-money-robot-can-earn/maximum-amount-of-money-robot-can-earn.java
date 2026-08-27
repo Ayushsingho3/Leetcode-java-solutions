40fc1dfd9b7985cdb64f842e4bb9c016f6497e74
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-08-27 22:35:57 +0530

    Time: 73 ms (91.03%) | Memory: 190.3 MB (37.69%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
@@ -2,42 +2,44 @@ class Solution {
     public int maximumAmount(int[][] coins) {
         int m = coins.length;
         int n = coins[0].length;
-        
         int[][][] dp = new int[m][n][3];
+        int MIN = Integer.MIN_VALUE / 2;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                java.util.Arrays.fill(dp[i][j], Integer.MIN_VALUE / 2);
+                dp[i][j][0] = MIN;
+                dp[i][j][1] = MIN;
+                dp[i][j][2] = MIN;
             }
         }
         
         dp[0][0][0] = coins[0][0];
-        if (coins[0][0] < 0) {
-            dp[0][0][1] = 0; 
-        }
+        dp[0][0][1] = coins[0][0] < 0 ? 0 : coins[0][0];
+        dp[0][0][2] = coins[0][0] < 0 ? 0 : coins[0][0];
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == 0 && j == 0) continue;
                 
+                int val = coins[i][j];
+                
                 for (int k = 0; k <= 2; k++) {
-                    int prev = Integer.MIN_VALUE / 2;
+                    int best = MIN;
                     
-                    if (i > 0) prev = Math.max(prev, dp[i - 1][j][k]);
-                    if (j > 0) prev = Math.max(prev, dp[i][j - 1][k]);
+                    if (i > 0) best = Math.max(best, dp[i - 1][j][k]);
+                    if (j > 0) best = Math.max(best, dp[i][j - 1][k]);
                     
-                    if (prev != Integer.MIN_VALUE / 2) {
-                        dp[i][j][k] = Math.max(dp[i][j][k], prev + coins[i][j]);
+                    if (best != MIN) {
+                        dp[i][j][k] = best + val;
                     }
                     
-                    if (k > 0 && coins[i][j] < 0) {
-                        int prev_k = Integer.MIN_VALUE / 2;
+                    if (val < 0 && k > 0) {
+                        int bestPrev = MIN;
+                        if (i > 0) bestPrev = Math.max(bestPrev, dp[i - 1][j][k - 1]);
+                        if (j > 0) bestPrev = Math.max(bestPrev, dp[i][j - 1][k - 1]);
                         
-                        if (i > 0) prev_k = Math.max(prev_k, dp[i - 1][j][k - 1]);
-                        if (j > 0) prev_k = Math.max(prev_k, dp[i][j - 1][k - 1]);
-                        
-                        if (prev_k != Integer.MIN_VALUE / 2) {
-                            dp[i][j][k] = Math.max(dp[i][j][k], prev_k);
+                        if (bestPrev != MIN) {
+                            dp[i][j][k] = Math.max(dp[i][j][k], bestPrev);
                         }
                     }
                 }
