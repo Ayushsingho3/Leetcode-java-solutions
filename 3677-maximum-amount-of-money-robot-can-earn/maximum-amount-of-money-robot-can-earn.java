8a853c7382c3b2c61a726956a8eea41d54a43e0f
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-08-28 23:53:44 +0530

    Time: 86 ms (56.89%) | Memory: 216.9 MB (17.29%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
@@ -2,50 +2,57 @@ class Solution {
     public int maximumAmount(int[][] coins) {
         int m = coins.length;
         int n = coins[0].length;
-        int[][][] dp = new int[m][n][3];
-        int MIN = Integer.MIN_VALUE / 2;
+        
+        long[][][] dp = new long[m][n][3];
+        long INF = Long.MIN_VALUE / 2;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                dp[i][j][0] = MIN;
-                dp[i][j][1] = MIN;
-                dp[i][j][2] = MIN;
+                for (int k = 0; k < 3; k++) {
+                    dp[i][j][k] = INF;
+                }
             }
         }
         
-        dp[0][0][0] = coins[0][0];
-        dp[0][0][1] = coins[0][0] < 0 ? 0 : coins[0][0];
-        dp[0][0][2] = coins[0][0] < 0 ? 0 : coins[0][0];
+        int v0 = coins[0][0];
+        dp[0][0][0] = v0;
+        if (v0 < 0) {
+            dp[0][0][1] = 0;
+        }
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == 0 && j == 0) continue;
                 
-                int val = coins[i][j];
+                long val = coins[i][j];
                 
-                for (int k = 0; k <= 2; k++) {
-                    int best = MIN;
+                for (int k = 0; k < 3; k++) {
+                    long maxPrev = INF;
+                    if (i > 0) maxPrev = Math.max(maxPrev, dp[i - 1][j][k]);
+                    if (j > 0) maxPrev = Math.max(maxPrev, dp[i][j - 1][k]);
                     
-                    if (i > 0) best = Math.max(best, dp[i - 1][j][k]);
-                    if (j > 0) best = Math.max(best, dp[i][j - 1][k]);
-                    
-                    if (best != MIN) {
-                        dp[i][j][k] = best + val;
+                    if (maxPrev != INF) {
+                        dp[i][j][k] = maxPrev + val;
                     }
                     
                     if (val < 0 && k > 0) {
-                        int bestPrev = MIN;
-                        if (i > 0) bestPrev = Math.max(bestPrev, dp[i - 1][j][k - 1]);
-                        if (j > 0) bestPrev = Math.max(bestPrev, dp[i][j - 1][k - 1]);
+                        long maxPrevK = INF;
+                        if (i > 0) maxPrevK = Math.max(maxPrevK, dp[i - 1][j][k - 1]);
+                        if (j > 0) maxPrevK = Math.max(maxPrevK, dp[i][j - 1][k - 1]);
                         
-                        if (bestPrev != MIN) {
-                            dp[i][j][k] = Math.max(dp[i][j][k], bestPrev);
+                        if (maxPrevK != INF) {
+                            dp[i][j][k] = Math.max(dp[i][j][k], maxPrevK);
                         }
                     }
                 }
             }
         }
         
-        return Math.max(dp[m - 1][n - 1][0], Math.max(dp[m - 1][n - 1][1], dp[m - 1][n - 1][2]));
+        long ans = INF;
+        for (int k = 0; k < 3; k++) {
+            ans = Math.max(ans, dp[m - 1][n - 1][k]);
+        }
+        
+        return (int) ans;
     }
 }
