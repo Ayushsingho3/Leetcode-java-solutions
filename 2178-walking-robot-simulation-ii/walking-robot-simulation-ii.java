aded26998a5ccca5f12dca93995abfabcf479ea0
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-08-29 23:28:32 +0530

    Time: 62 ms (46.73%) | Memory: 58 MB (37.38%) - LeetSync

--- 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
@@ -8,36 +8,36 @@ class Robot {
     public Robot(int width, int height) {
         this.w = width;
         this.h = height;
-        this.p = 2 * (w - 1) + 2 * (h - 1);
+        this.p = 2 * (width + height - 2);
         this.pos = 0;
         this.moved = false;
     }
-    
+
     public void step(int num) {
-        moved = true;
-        pos = (pos + num) % p;
+        this.pos = (this.pos + num) % this.p;
+        this.moved = true;
     }
-    
+
     public int[] getPos() {
-        if (pos < w) {
+        if (pos <= w - 1) {
             return new int[]{pos, 0};
-        } else if (pos < w + h - 1) {
+        } else if (pos <= w + h - 2) {
             return new int[]{w - 1, pos - (w - 1)};
-        } else if (pos < 2 * w + h - 2) {
+        } else if (pos <= 2 * w + h - 3) {
             return new int[]{w - 1 - (pos - (w + h - 2)), h - 1};
         } else {
             return new int[]{0, h - 1 - (pos - (2 * w + h - 3))};
         }
     }
-    
+
     public String getDir() {
         if (pos == 0) {
             return moved ? "South" : "East";
-        } else if (pos < w) {
+        } else if (pos <= w - 1) {
             return "East";
-        } else if (pos < w + h - 1) {
+        } else if (pos <= w + h - 2) {
             return "North";
-        } else if (pos < 2 * w + h - 2) {
+        } else if (pos <= 2 * w + h - 3) {
             return "West";
         } else {
             return "South";
