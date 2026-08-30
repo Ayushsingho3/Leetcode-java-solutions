a700ad87b1ee991dc207435f387cb08e5984d89e
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-08-30 23:34:32 +0530

    Time: 54 ms (95.00%) | Memory: 186.2 MB (71.47%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.java
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.java
@@ -1,32 +1,32 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public List<Integer> solveQueries(int[] nums, int[] queries) {
         int n = nums.length;
-        Map<Integer, List<Integer>> indexMap = new HashMap<>();
+        Map<Integer, List<Integer>> pos = new HashMap<>();
         
         for (int i = 0; i < n; i++) {
-            indexMap.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+            pos.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
         }
         
         int[] minDist = new int[n];
         Arrays.fill(minDist, -1);
         
-        for (List<Integer> indices : indexMap.values()) {
-            int k = indices.size();
+        for (List<Integer> list : pos.values()) {
+            int k = list.size();
             if (k > 1) {
-                for (int j = 0; j < k; j++) {
-                    int prev = (j - 1 + k) % k;
-                    int next = (j + 1) % k;
+                for (int m = 0; m < k; m++) {
+                    int current = list.get(m);
+                    int prev = list.get((m - 1 + k) % k);
+                    int next = list.get((m + 1) % k);
                     
-                    int d1 = (indices.get(j) - indices.get(prev) + n) % n;
-                    int d2 = (indices.get(next) - indices.get(j) + n) % n;
+                    int d1 = Math.abs(current - prev);
+                    d1 = Math.min(d1, n - d1);
                     
-                    minDist[indices.get(j)] = Math.min(d1, d2);
+                    int d2 = Math.abs(current - next);
+                    d2 = Math.min(d2, n - d2);
+                    
+                    minDist[current] = Math.min(d1, d2);
                 }
             }
         }
