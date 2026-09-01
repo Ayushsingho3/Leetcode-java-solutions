83f96e47d9bd9c2704f5aa9ca8a1329fdffabce4
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-09-01 20:27:00 +0530

    Time: 32 ms (80.98%) | Memory: 60.6 MB (95.84%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.java
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -3,42 +3,43 @@ import java.util.Set;
 
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {1, 0, -1, 0};
-        int x = 0;
-        int y = 0;
-        int direction = 0;
-        
-        Set<Long> obsSet = new HashSet<>();
+        Set<Long> obstacleSet = new HashSet<>();
         for (int[] obs : obstacles) {
-            obsSet.add(((long) obs[0] << 32) | (obs[1] & 0xFFFFFFFFL));
+            long hash = (((long) obs[0]) << 32) | ((long) obs[1] & 0xFFFFFFFFL);
+            obstacleSet.add(hash);
         }
         
-        int maxDistSq = 0;
+        // Directions: North (0), East (1), South (2), West (3)
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int d = 0;
+        
+        int x = 0;
+        int y = 0;
+        int maxDistSquared = 0;
         
         for (int cmd : commands) {
-            if (cmd == -2) {
-                direction = (direction + 3) % 4;
-            } else if (cmd == -1) {
-                direction = (direction + 1) % 4;
+            if (cmd == -1) {
+                d = (d + 1) % 4;
+            } else if (cmd == -2) {
+                d = (d + 3) % 4;
             } else {
                 for (int i = 0; i < cmd; i++) {
-                    int nx = x + dx[direction];
-                    int ny = y + dy[direction];
+                    int nx = x + dirs[d][0];
+                    int ny = y + dirs[d][1];
                     
-                    long hash = ((long) nx << 32) | (ny & 0xFFFFFFFFL);
+                    long nextHash = (((long) nx) << 32) | ((long) ny & 0xFFFFFFFFL);
                     
-                    if (obsSet.contains(hash)) {
+                    if (obstacleSet.contains(nextHash)) {
                         break;
                     }
                     
                     x = nx;
                     y = ny;
-                    maxDistSq = Math.max(maxDistSq, x * x + y * y);
+                    maxDistSquared = Math.max(maxDistSquared, x * x + y * y);
                 }
             }
         }
         
-        return maxDistSq;
+        return maxDistSquared;
     }
 }
