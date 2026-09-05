28c96c4be268fc3625d436d5ac5421e2f30e7942
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-09-05 23:19:24 +0530

    Time: 56 ms (29.72%) | Memory: 72.7 MB (26.51%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.java
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.java
@@ -1,33 +1,42 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class Solution {
     public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
         Collections.sort(robot);
         Arrays.sort(factory, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        List<Integer> factoryPositions = new ArrayList<>();
+
+        List<Integer> slots = new ArrayList<>();
         for (int[] f : factory) {
-            for (int i = 0; i < f[1]; i++) {
-                factoryPositions.add(f[0]);
+            int pos = f[0];
+            int limit = Math.min(f[1], robot.size());
+            for (int i = 0; i < limit; i++) {
+                slots.add(pos);
             }
         }
-        
+
         int n = robot.size();
-        long[] dp = new long[n + 1];
-        long INF = (long) 1e18; 
-        
-        Arrays.fill(dp, INF);
-        dp[0] = 0;
-        
-        for (int fPos : factoryPositions) {
-            for (int i = n; i >= 1; i--) {
-                if (dp[i - 1] != INF) {
-                    long distance = Math.abs((long) robot.get(i - 1) - fPos);
-                    dp[i] = Math.min(dp[i], dp[i - 1] + distance);
+        int m = slots.size();
+
+        long[][] dp = new long[n + 1][m + 1];
+        long INF = 1000000000000000L;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = INF;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = dp[i][j - 1];
+                long cost = dp[i - 1][j - 1] + Math.abs((long) robot.get(i - 1) - slots.get(j - 1));
+                if (cost < dp[i][j]) {
+                    dp[i][j] = cost;
                 }
             }
         }
-        
-        return dp[n];
+
+        return dp[n][m];
     }
 }
