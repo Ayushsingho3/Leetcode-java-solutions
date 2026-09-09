7b091680a7af7430a8e6540c28d028f83f2810e2
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-09-09 22:49:44 +0530

    Time: 67 ms (41.59%) | Memory: 118 MB (95.38%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
@@ -19,6 +19,9 @@ class Solution {
     }
 
     private int reverse(int x) {
+        if (x == 0) {
+            return 0;
+        }
         int rev = 0;
         while (x > 0) {
             rev = rev * 10 + (x % 10);
