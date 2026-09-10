a8d6f9274acc8fe0cbfdd809231b9d51e76551d3
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-09-10 23:40:35 +0530

    Time: 9 ms (97.22%) | Memory: 124.2 MB (87.37%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
@@ -2,25 +2,23 @@ class Solution {
     public boolean containsCycle(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[] parent = new int[m * n];
-        for (int i = 0; i < m * n; i++) {
-            parent[i] = i;
-        }
+
+        DSU dsu = new DSU(m * n);
 
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
-                int curr = r * n + c;
+                int u = r * n + c;
 
                 if (c + 1 < n && grid[r][c] == grid[r][c + 1]) {
-                    int right = r * n + c + 1;
-                    if (!union(parent, curr, right)) {
+                    int v = r * n + (c + 1);
+                    if (!dsu.union(u, v)) {
                         return true;
                     }
                 }
 
                 if (r + 1 < m && grid[r][c] == grid[r + 1][c]) {
-                    int down = (r + 1) * n + c;
-                    if (!union(parent, curr, down)) {
+                    int v = (r + 1) * n + c;
+                    if (!dsu.union(u, v)) {
                         return true;
                     }
                 }
@@ -30,20 +28,31 @@ class Solution {
         return false;
     }
 
-    private int find(int[] parent, int i) {
-        if (parent[i] == i) {
-            return i;
-        }
-        return parent[i] = find(parent, parent[i]);
-    }
+    private static class DSU {
+        private final int[] parent;
 
-    private boolean union(int[] parent, int i, int j) {
-        int rootI = find(parent, i);
-        int rootJ = find(parent, j);
-        if (rootI == rootJ) {
-            return false;
+        public DSU(int size) {
+            parent = new int[size];
+            for (int i = 0; i < size; i++) {
+                parent[i] = i;
+            }
         }
-        parent[rootI] = rootJ;
-        return true;
+
+        public int find(int i) {
+            if (parent[i] == i) {
+                return i;
+            }
+            return parent[i] = find(parent[i]);
+        }
+
+        public boolean union(int i, int j) {
+            int rootI = find(i);
+            int rootJ = find(j);
+            if (rootI == rootJ) {
+                return false;
+            }
+            parent[rootI] = rootJ;
+            return true;
+        }
     }
 }
