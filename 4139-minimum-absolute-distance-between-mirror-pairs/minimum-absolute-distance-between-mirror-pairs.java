63d8ea4af3e03e7584debf724529120583cefdd0
AYUSH SINGH <ayushsingh1382004@gmail.com>
2026-09-11 22:49:28 +0530

    Time: 65 ms (59.08%) | Memory: 118.6 MB (32.36%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
@@ -3,29 +3,25 @@ import java.util.Map;
 
 class Solution {
     public int minMirrorPairDistance(int[] nums) {
-        Map<Integer, Integer> lastSeen = new HashMap<>();
-        int minDist = Integer.MAX_VALUE;
+        Map<Integer, Integer> map = new HashMap<>();
+        int minDistance = Integer.MAX_VALUE;
 
         for (int j = 0; j < nums.length; j++) {
-            if (lastSeen.containsKey(nums[j])) {
-                minDist = Math.min(minDist, j - lastSeen.get(nums[j]));
+            if (map.containsKey(nums[j])) {
+                minDistance = Math.min(minDistance, j - map.get(nums[j]));
             }
-            
             int rev = reverse(nums[j]);
-            lastSeen.put(rev, j);
+            map.put(rev, j);
         }
 
-        return minDist == Integer.MAX_VALUE ? -1 : minDist;
+        return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
     }
 
-    private int reverse(int x) {
-        if (x == 0) {
-            return 0;
-        }
+    private int reverse(int n) {
         int rev = 0;
-        while (x > 0) {
-            rev = rev * 10 + (x % 10);
-            x /= 10;
+        while (n > 0) {
+            rev = rev * 10 + n % 10;
+            n /= 10;
         }
         return rev;
     }
